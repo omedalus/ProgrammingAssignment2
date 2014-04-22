7fff6323454efffa620aba8922ff0bd39fda83c8
Mikhail Voloshin <mvol@live.com>
2014-04-22 12:02:47 -0400

    cached matrix implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates an object that stores a matrix along with its inverse. This object
+## can be passed to the cacheSolve function, which will get/set the cached
+## inverse of the matrix. This is more computationally efficient than
+## computing the inverse every time it's requested.
 
 makeCacheMatrix <- function(x = matrix()) {
+        # Private ember variable that will store the inverse of the x matrix.
+        cached.inverse <- NULL
 
+        set <- function(y) {
+                x <<- y
+
+                # Now that we've changed the matrix stored in this object,
+                # our cached inverse is invalid.
+                cached.inverse <<- NULL
+        }
+
+        get <- function() { 
+                x 
+        }
+
+        setinverse <- function(inverse) {
+                cached.inverse <<- inverse
+        }
+        
+        getinverse <- function() {
+                cached.inverse
+        }
+
+        list(set = set, 
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of the matrix stored in x, which should be an object
+## created by makeCacheMatrix. Uses x's cached inverse if it's available,
+## or computes a fresh inverse if it's not.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Ask the x object for its inverse. This action does not actually
+        # compute the inverse; it merely retrieves the x object's
+        # cached.inverse member.
+        inverse <- x$getinverse()
+
+        if(!is.null(inverse)) {
+                # Cache hit.
+                # The x object has a cached inverse from a previous call
+                # to this method. Hooray. Return the cached value.
+                message("getting cached data")
+                return(inverse)
+        }
+
+        # Cache miss. We have to compute the x object's matrix's inverse.
+        data <- x$get()
+        inverse <- solve(data, ...)
+        
+        # Cache our solution inside x so we won't have to perform this
+        # potentially expensive computation again.
+        x$setinverse(inverse)
+        
+        # Return the computed value.
+        inverse
 }
